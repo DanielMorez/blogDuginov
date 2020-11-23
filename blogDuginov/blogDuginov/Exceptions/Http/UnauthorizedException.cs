ed423d8061ff33715a5c169bdc7cb9d20e46da42
DanielMorez <denisduginov17@gmail.com>
2020-11-23 12:52:52 +0400

    Без гарантеров

---
+++ blogDuginov/blogDuginov/Exceptions/Http/UnauthorizedException.cs
@@ -0,0 +1,27 @@
+﻿using blogDuginov.Exceptions.Http;
+using Microsoft.AspNetCore.Http;
+using WebApplication3.Exceptions.Http;
+
+namespace BlogForStudents.Exceptions.Http
+{
+    /// <summary>
+    /// Ошибка авторизации (код 401)
+    /// </summary>
+    public class UnauthorizedException : HttpException
+    {
+        /// <summary>
+        /// Создание экземпляра класса <see cref="UnauthorizedException"/>
+        /// </summary>
+        /// <param name="errorObject">Объект описания ошибки</param>
+        public UnauthorizedException(object errorObject)
+            : base(StatusCodes.Status401Unauthorized, errorObject)
+        { }
+
+        /// <summary>
+        /// Создание экземпляра класса <see cref="UnauthorizedException"/>
+        /// </summary>
+        public UnauthorizedException()
+            : this(null)
+        { }
+    }
+}
